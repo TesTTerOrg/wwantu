9677ca629cddecad41afbdafd7121da0a5698129
andresns <andresm.navarro@gmail.com>
2020-10-08 13:04:48 -0400

    Add feature to remove movies from favorites, add toast when movie is added to favorite, check if movie is already in favorite

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require("./db/mongoose");
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
-const omdbapi = require("./utils/omdbapi.js");
+const omdbapi = require("./utils/omdbapi");
 
 //Models
 const Movie = require("./models/movie");
@@ -31,22 +31,39 @@ app.get("/results", (req, res) => {
         return res.render("results", {
             error: "Debe ingresar el nombre de la película.",
             search: undefined,
-            results: undefined
+            results: undefined,
+            fail: false
         });
     }
+
     omdbapi(req.query.title, "", (error, results) => {
         if (error) {
             return res.render("results", {
                 error: error,
                 search: req.query.title,
-                results: undefined
+                results: undefined,
+                fail: false
             });
         }
+
+        // results.map((r) => {
+        //     Movie.findOne({
+        //         imdbID: r.imdbID
+        //     }, (error, movie) => {
+        //         if (error) {
+        //             return console.log(error);
+        //         }
+        //         if (movie) {
+        //             r.added = true;
+        //         }
+        //     });
+        // });
 
         res.render("results", {
             error: undefined,
             search: req.query.title,
-            results: results
+            results: results,
+            fail: false
         });
     });
 });
@@ -56,29 +73,53 @@ app.get("/favorite", (req, res) => {
         return res.status(500).send("Debe ingresar el ID de IMDB.");
     }
 
-    omdbapi("", req.query.imdbID, (error, results) => {
+    Movie.findOne({
+        imdbID: req.query.imdbID
+    }, (error, movie) => {
         if (error) {
-            return res.render("results", {
-                error: error,
-                search: req.query.title,
-                results: undefined
+            return res.status(500).send();
+        }
+        if (movie) {
+            res.status(600).send();
+        } else {
+            omdbapi("", req.query.imdbID, (error, results) => {
+                if (error) {
+                    return res.render("results", {
+                        error: error,
+                        search: req.query.title,
+                        results: undefined,
+                        fail: false
+                    });
+                }
+
+                const movie = new Movie({
+                    imdbID: results.imdbID,
+                    title: results.Title,
+                    year: results.Year,
+                    imageURL: results.Poster
+                });
+
+                movie.save().then(() => {
+                    res.redirect("back");
+                }).catch((e) => {
+                    res.status(500).send(e);
+                });
             });
         }
-
-        const movie = new Movie({
-            imdbID: results.imdbID,
-            title: results.Title,
-            year: results.Year,
-            imageURL: results.Poster
-        });
-
-        movie.save().then(() => {
-            res.redirect("back");
-        }).catch((e) => {
-            res.status(400).send(e);
-        });
     });
+});
 
+app.get("/remove", (req, res) => {
+    if (!req.query.id) {
+        return res.status(500).send("No se encontró un ID.");
+    }
+    Movie.deleteOne({
+        _id: req.query.id
+    }).then(() => {
+        res.redirect("back");
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.get("/favorites", (req, res) => {
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/testermovies",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 });
